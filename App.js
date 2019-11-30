decbfd0063b33d385ee1d22e68888234703d1226
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-11-30 15:53:58 +0100

    Get phone coordinates

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    errorMessage: null,
+    longitude: null,
+    latitude: null,
+  };
+
+  componentWillMount () {
+    this.getLocationAsync();
+  }
+
+  getLocationAsync = async() => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    const longitude = location.coords.longitude;
+    const latitude = location.coords.latitude;
+    this.setState({ 
+      longitude: longitude,
+      latitude: latitude,
+     });
+  } 
+  render() {
+    let text = 'Waiting...';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.longitude && this.state.latitude) {
+      text = "Longitude: " + this.state.longitude + "\n" + "Latitude: " + this.state.latitude;
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>{text}</Text>
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +53,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'right',
   },
 });
