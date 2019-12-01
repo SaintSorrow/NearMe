eb4a91460ded7f6a7c80891cdf461d1905afb386
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-12-01 19:53:42 +0100

    Display list of location and open a webpage after a click

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ScrollView 
+} from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
+import WikipediaArticle from './Components/WikipediaArticle';
 
 export default class App extends Component {
   state = {
     errorMessage: null,
     longitude: null,
     latitude: null,
+    pages: null,
   };
+
+  async nearbyPages(latitude, longitude) {
+    let url = "https://en.wikipedia.org/w/api.php";
+    let params = {
+      action: "query",
+      list: "geosearch",
+      gscoord: latitude + "|" + longitude,
+      gsradius: "10000",
+      gslimit: "10",
+      format: "json"
+    }
+  
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){
+      url += "&" + key + "=" + params[key];
+    });
+  
+    try {
+      let respond = await fetch(url);
+      respond = await respond.json();
+      let formattedPages = [];
+      const pages = respond.query.geosearch;
+
+      pages.forEach((item) => {
+        let newPage = {
+          title: item.title,
+          pageId: item.pageid,
+          distance: item.dist
+        };
+        formattedPages.push(newPage);
+      })
+
+      return formattedPages;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   componentWillMount () {
     this.getLocationAsync();
   }
 
-  getLocationAsync = async() => {
+  async getLocationAsync() {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
@@ -23,24 +67,21 @@ export default class App extends Component {
     }
 
     const location = await Location.getCurrentPositionAsync({});
-    const longitude = location.coords.longitude;
-    const latitude = location.coords.latitude;
+    const pages = await this.nearbyPages(location.coords.latitude, location.coords.longitude);
     this.setState({ 
-      longitude: longitude,
-      latitude: latitude,
+      longitude: location.coords.longitude,
+      latitude: location.coords.latitude,
+      pages: pages,
      });
   } 
-  render() {
-    let text = 'Waiting...';
-    if (this.state.errorMessage) {
-      text = this.state.errorMessage;
-    } else if (this.state.longitude && this.state.latitude) {
-      text = "Longitude: " + this.state.longitude + "\n" + "Latitude: " + this.state.latitude;
-    }
 
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>{text}</Text>
+        <ScrollView>
+          {this.state.pages != null && (this.state.pages.map(page => <WikipediaArticle page={page} key={page.pageId}/>))}
+          {this.state.pages == null && (<Text>Loading...</Text>)}
+        </ScrollView>
       </View>
     );
   }
---
+++ Components/WikipediaArticle.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking
+} from 'react-native';
+
+export default class WikipediaArticle extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      page: props.page,
+    }
+  }
+
+  openLink = () => {
+    Linking.openURL("http://en.wikipedia.org/?curid=" + this.state.page.pageId);
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.openLink}>
+          <Text style={styles.text}>{this.state.page.title}</Text>
+          <Text style={styles.text}>Distance: {this.state.page.distance}</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 16,
+  }
+})
